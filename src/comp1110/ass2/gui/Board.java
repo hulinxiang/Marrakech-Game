32d841ef84b9a9556316dc742a4d7a0da17ba5cd
u7679626 <u7679626@any.edu.au>
2023-08-20 20:43:17 +1000

    Board class skeleton completed.

---
+++ src/comp1110/ass2/gui/Board.java
@@ -0,0 +1,26 @@
+package comp1110.ass2.gui;
+/*
+This class defines the shape (width and height) of the game board.
+ */
+public class Board {
+
+    public final static int BOARD_HEIGHT = 7;
+    public final static int BOARD_WIDTH = 7;
+
+    Tile[][] tiles; //Array of tiles that the board is made up of.
+    Merchant merchant;
+
+    /*
+    Sets up the initial state of the board.
+     */
+    public static void boardSetUp(){
+
+    }
+
+    /*
+    Denotes the location of the mosaic circles on the board.
+     */
+    public static void mosaic(){
+
+    }
+}
