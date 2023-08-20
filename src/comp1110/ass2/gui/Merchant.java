a3db42217006d68faa7a0ac1abaa329ecea8dc3b
u7679626 <u7679626@any.edu.au>
2023-08-20 19:57:49 +1000

    Merchant class skeleton completed.

---
+++ src/comp1110/ass2/gui/Merchant.java
@@ -0,0 +1,45 @@
+package comp1110.ass2.gui;
+
+
+public class Merchant {
+    int x; //X coordinate position of merchant on the board
+    int y; //Y coordinate position of merchant on the board
+
+    Direction direction;
+
+    Merchant(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /*
+    Sets the initial direction of the merchant at the start of the game.
+     */
+    public void firstDirection(Direction d){
+        this.direction = d;
+
+    }
+
+    /*
+    Rotates the merchant depending on the player input.
+    d: The current direction the merchant is facing.
+    rotateValue: -1 = left rotation, 0 = no rotation, 1 = right rotation.
+     */
+    public void Rotate(Direction d, int rotateValue){
+
+    }
+
+    /*
+    Moves the required number of steps as indicated by the die, in the specific direction.
+     */
+    public void Move(Direction d, int steps){
+
+    }
+
+    /*
+    Checks that the rotation is no more than 90 degrees. Returns false if illegal rotation, returns true if legal.
+     */
+    public static boolean checkRotation(Direction d){
+        return true;
+    }
+}
