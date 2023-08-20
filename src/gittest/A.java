a5889c5607da2514dec4b55a9b09846e2ada0fbf
Alice <u7679626@anu.edu.au>
2023-08-21 08:26:27 +1000

    Alice 1st task done.

---
+++ src/gittest/A.java
@@ -0,0 +1,9 @@
+package gittest;
+
+public class A {
+
+    @Override
+    public String toString(){
+        return "";
+    }
+}
---
+++ src/gittest/Main.java
@@ -0,0 +1,7 @@
+package gittest;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
