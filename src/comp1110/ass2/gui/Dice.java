b8963faf93c487e5ca0112df6a265ee34fada67a
u7679626 <u7679626@any.edu.au>
2023-08-20 19:31:24 +1000

    Dice class skeleton completed.

---
+++ src/comp1110/ass2/gui/Dice.java
@@ -0,0 +1,17 @@
+package comp1110.ass2.gui;
+
+/*
+Defines the behaviour of the dice.
+ */
+public class Dice {
+    int[] diceValues = {1, 2, 2, 3, 3, 4};
+    //Array of integers denoting dice set-up (six sided with 1 one and four, and 2 twos and threes).
+
+    /*
+    Rolls a value according to the dice set up to decide who plays first and the movement of the merchant.
+     */
+    public static int Roll(){
+        int rolledInt = 0; //Number rolled between 1-4
+        return rolledInt;
+    }
+}
