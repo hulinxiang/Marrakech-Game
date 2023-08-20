3a024a648d31d57c0dbede6c5c99d35318105c5d
u7679626 <u7679626@any.edu.au>
2023-08-20 20:28:56 +1000

    Rug class skeleton completed.

---
+++ src/comp1110/ass2/gui/Rug.java
@@ -0,0 +1,31 @@
+package comp1110.ass2.gui;
+/*
+Behaviour of the rug on the board
+ */
+public class Rug {
+
+    int[][] rugPosition; //Denotes rug position. HORIZONTAL (X1, Y), (X2, Y) VERTICAL (X, Y1), (X, Y2).
+
+    /*
+    Stores the coordinates of where the rug is placed.
+     */
+    public void rugPlacement(){
+
+    }
+
+    /*
+    Checks that the rug shares a border with the merchant
+     */
+    public boolean checkShareBoarder(){
+        return true;
+    }
+
+    /*
+    Checks that the rug is not placed outside the bounds of the board.
+     */
+    public boolean correctPlacement(){
+        return true;
+    }
+
+
+}
